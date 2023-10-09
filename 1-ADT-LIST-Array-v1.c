ab5c63102f9a1fab1bb915f8e0c03cb634a470fd
hello-isa <isabeljoyadriatico@gmail.com>
2023-10-09 15:42:22 +0800

    delete and isMember

--- 1-ADT-LIST-Array-v1.c
+++ 1-ADT-LIST-Array-v1.c
@@ -8,28 +8,47 @@ typedef struct
     int count; // Keep track of the number of elements
 } LIST;
 
+typedef enum
+{
+    F,
+    T
+} boolean;
+
+// Function declaration
 void initialize(LIST *L);             // Initialize to empty list
 void display(LIST L);                 // Display the content of the list
 void insert(LIST *L, int x, int pos); // Insert an element at any position
 void delete(LIST *L, int pos);        // Delete at any position
+boolean isMember(LIST L, int x);
 
 int main()
 {
     LIST A;
 
     initialize(&A);
+    printf("Initialize empty array:\n");
+    display(A);
 
+    printf("\nAfter insertion:\n");
     insert(&A, 11, 0);
     insert(&A, 12, 1);
     insert(&A, 13, 2);
     insert(&A, 14, 3);
     insert(&A, 15, 4);
-    printf("Insertion:\n");
     display(A);
+
+    printf("\nAfter deletion:\n");
+    delete (&A, 2);
+    delete (&A, 1);
+    display(A);
+
+    int y = 14;
+    (isMember(A, y) == T) ? printf("\n%d is a member of the list.", y) : printf("\n%d is NOT a member of the list.", y);
 
     return 0;
 }
 
+// Function definition
 void initialize(LIST *L)
 {
     L->count = 0;
@@ -73,5 +92,31 @@ void insert(LIST *L, int x, int pos)
 
 void delete(LIST *L, int pos)
 {
-    if (pos < 0 && pos)
+    if (pos < 0 || pos > L->count)
+    {
+        printf("Invalid position for deletion.");
+    }
+    else
+    {
+        int n;
+        for (n = pos; n < L->count; n++)
+        {
+            L->elem[n] = L->elem[n + 1]; // Shifting
+        }
+        L->count--;
+    }
+}
+
+boolean isMember(LIST L, int x)
+{
+    int n;
+    for (n = 0; n < L.count; n++)
+    {
+        if (x == L.elem[n])
+        {
+            return T;
+        }
+    }
+    return F; // This is outside of the loop so that this statement will only be executed once the loop has gone through the list and didn't found any match
+    // Placing this inside the loop will return F as soon as it encounters element that does not match x which is incorrect if x is present later in the list
 }
