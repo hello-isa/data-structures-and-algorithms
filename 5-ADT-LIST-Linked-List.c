bb6e7d1254712ad72313374b16171be296e0db2a
hello-isa <isabeljoyadriatico@gmail.com>
2023-10-12 12:21:19 +0800

    Add delete at head, tail, all occurrences and check if element is a member

--- 5-ADT-LIST-Linked-List.c
+++ 5-ADT-LIST-Linked-List.c
@@ -8,12 +8,22 @@ typedef struct node
     struct node *link;
 } *LIST;
 
+typedef enum
+{
+    F,
+    T
+} boolean;
+
 void initialize(LIST *L);              // Initialize empty list
 void display(LIST L);                  // Display the content of the list
 LIST createNode(int value);            // Create a new node which is needed every time you insert
 void insertHead(LIST *L, int value);   // Insert a new element at the head of the list
 void insertTail(LIST *L, int value);   // Insert a new element at the tail of the list
-void insertSorted(LIST *L, int value); // Insert a new element in a sorted linked list
+void insertSorted(LIST *L, int value); // Insert a new element in a sorted list
+void deleteHead(LIST *L);              // Delete the first element (head) of the list
+void deleteTail(LIST *L);              // Delete the last element(tail) of the list
+void deleteOccur(LIST *L, int x);      // Delete all occurrences of an element in the list
+boolean isMember(LIST L, int y);       // Check if an element is a member of the list
 
 int main()
 {
@@ -22,18 +32,36 @@ int main()
     initialize(&A);
 
     printf("Insert at head:\n");
-    insertHead(&A, 10);
+    insertHead(&A, 20);
+    insertHead(&A, 20);
     insertHead(&A, 5);
     display(A);
 
     printf("\nInsert at tail:\n");
     insertTail(&A, 42);
+    insertTail(&A, 100);
     display(A);
 
     int x = 32;
     printf("\nInsert %d in a sorted list:\n", x);
     insertSorted(&A, x);
     display(A);
+
+    printf("\nDelete head:\n");
+    deleteHead(&A);
+    display(A);
+
+    printf("\nDelete tail:\n");
+    deleteTail(&A);
+    display(A);
+
+    int deleteAll = 20;
+    printf("\nDelete all occurrences of %d:\n", deleteAll);
+    deleteOccur(&A, deleteAll);
+    display(A);
+
+    int member = 42;
+    (isMember(A, member) == T) ? printf("\n%d is a member of the list.\n", member) : printf("\n%d is NOT a member of the list.\n", member);
 
     return 0;
 }
@@ -119,4 +147,54 @@ void insertSorted(LIST *L, int value)
         newNode->link = *current;
         *current = newNode;
     }
+}
+void deleteHead(LIST *L)
+{
+    LIST *temp;
+    temp = L;
+    *L = (*temp)->link;
+    free(temp);
+}
+
+void deleteTail(LIST *L)
+{
+    LIST *current, *temp;
+    // To know the last element, the link is NULL
+    for (current = L; (*current)->link != NULL; current = &(*current)->link)
+    {
+    }
+    temp = current;
+    *current = (*temp)->link;
+    free(temp);
+}
+
+void deleteOccur(LIST *L, int x)
+{
+    LIST *current, *temp;
+    for (current = L; (*current) != NULL;)
+    {
+        if ((*current)->data == x)
+        {
+            temp = current;
+            *current = (*temp)->link;
+            free(temp);
+        }
+        else
+        {
+            current = &(*current)->link;
+        }
+    }
+}
+
+boolean isMember(LIST L, int y)
+{
+    LIST current;
+    for (current = L; current != NULL; current = current->link)
+    {
+        if (current->data == y)
+        {
+            return T;
+        }
+    }
+    return F;
 }
